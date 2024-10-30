e0af0cb2f0abe29a93115eac33060a29475e1f0f
Xedice Abbasova <abbasovaxdc@gmail.com>
2024-10-30 23:40:24 +0400

    Initial commit

--- src/main/java/com/khadija/oauth/controller/LoginController.java
+++ src/main/java/com/khadija/oauth/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.khadija.oauth.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,10 +16,22 @@ public class LoginController {
 
     @GetMapping("/login")
     public String login(HttpServletRequest request, Model model, String error, String logout) {
+
         if (error != null) {
             model.addAttribute("error", true);
             model.addAttribute("errorMessage", "Invalid username or password");
         }
+
+        if (logout != null) {
+            model.addAttribute("logout", true);
+            model.addAttribute("logoutMessage", "You have been logged out successfully");
+        }
+
+        CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+        if (csrf != null) {
+            model.addAttribute("csrf", csrf);
+        }
+
         return "pages/login";
     }
 }
